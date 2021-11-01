ef7c1c31842702ea4797cff243dc8ea9d858e2b1
rpamith <92911526+rpamith@users.noreply.github.com>
2021-11-01 13:12:23 +0530

    Add files via upload

---
+++ Java Assignment-3 LengthOfString/LengthOfString.java
@@ -0,0 +1,13 @@
+package LengthOfString;
+
+public class LengthOfString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="hello world";
+		
+		System.out.println(a.length());
+
+	}
+
+}
