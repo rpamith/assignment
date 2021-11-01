00e3bf4f4cfb72f0544f1c23f7764d2a6ca2c4c9
rpamith <92911526+rpamith@users.noreply.github.com>
2021-11-01 13:05:51 +0530

    Add files via upload

---
+++ SimpleInterest.java
@@ -0,0 +1,17 @@
+package SimpleInterest;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		int p=10000;
+		double SI;
+		double r=5;
+		double n=2.5;
+		
+		SI=(p*n*r)/100;
+		
+		System.out.println("Calculate the simple interest is:"+SI);
+
+	}
+
+}
