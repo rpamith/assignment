6c281649ff94ca74562ca033919304679b38cea9
rpamith <92911526+rpamith@users.noreply.github.com>
2021-11-01 13:12:06 +0530

    Add files via upload

---
+++ Java Assignment-3 JoinString/JoinString.java
@@ -0,0 +1,17 @@
+package JoinString;
+
+public class JoinString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a="hello";
+		String b="How are you?";
+		
+		String joined_string = a.concat("!").concat(b);
+		
+		System.out.println(joined_string);
+
+	}
+
+}
