0ee434aa935bf3bf0abe0eae1d4747a61905f91d
rpamith <92911526+rpamith@users.noreply.github.com>
2021-11-09 11:29:59 +0530

    Add files via upload

---
+++ Java-11-Http/Http.java
@@ -0,0 +1,30 @@
+package Http;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class Http {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		 String getEndpoint = "http://httpbin.org/get";
+
+	        var request = HttpRequest.newBuilder()
+	            .uri(URI.create(getEndpoint))
+	            .GET()
+	            .build();
+
+	        var client = HttpClient.newHttpClient();
+
+	        HttpResponse <String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+	        System.out.println("Status Code: " + response.statusCode());
+	        System.out.println("Response: " + response.body());
+
+	}
+
+}
